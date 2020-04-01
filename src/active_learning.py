ff4052c979c1b6875ccfc89e718620a9253dc934
sorenmulli <swholm@protonmail.com>
2020-04-01 22:03:21 +0200

    Report fixing

--- src/active_learning.py
+++ src/active_learning.py
@@ -16,25 +16,25 @@ def dataprepare(bins=4, split=0.6):
 		X, y = data['X'], data['y']
 	with open('src/saved_data/names.json', 'r') as infile:
 		namedata = json.load(infile)
-		
+
 	country_names,feature_names = namedata['countries'], namedata['features']
 
 	y =  KBinsDiscretizer(n_bins=bins, encode='ordinal', strategy='quantile').fit_transform(y.reshape(-1, 1))
 	X_last = OneHotEncoder(categories='auto', sparse=False).fit_transform(X[:, -1].reshape(-1, 1))
-	X = StandardScaler().fit_transform(X[:, :-1]) 
-	
+	X = StandardScaler().fit_transform(X[:, :-1])
+
 	X = np.hstack( (X, X_last) )
-	
+
 	scrampled_idcs = np.random.permutation(X.shape[0])
 	cutoff = int(split*X.shape[0])
 	pool_idcs, test_idcs = list(scrampled_idcs[:cutoff]), scrampled_idcs[cutoff:]
-	
+
 	#There needs to be examples of each class in both test and train
 	assert len(np.unique(y[test_idcs])) == bins and len(np.unique(y[pool_idcs])) == bins
 
 	return X, y.flatten(), pool_idcs, test_idcs, country_names, feature_names
 
-	
+
 random = lambda probs: 0
 least_conf = lambda probs: np.argsort(1 - probs.max(1))[0]
 margin = lambda probs: np.argsort(probs.max(1)-probs.min(1))[-1]
@@ -52,14 +52,14 @@ def train_on_pool(choice_function, X, y,  pool_idcs, train_idcs, test_idcs, name
 	gp = GaussianProcessClassifier(n_restarts_optimizer=25, kernel=Matern(),  n_jobs=-1, random_state=42)
 
 	#Add initial points
-	
+
 	while pool_idcs:
 		Xtrain, ytrain = X[train_idcs], y[train_idcs]
 		gp.fit(Xtrain, ytrain)
-		
+
 		preds = gp.predict(Xtest)
-		 
-		accuracies.append(accuracy_score(ytest, preds)) 
+
+		accuracies.append(accuracy_score(ytest, preds))
 		n_points.append( len(train_idcs) )
 
 		train_classes = np.unique(y[train_idcs], return_counts = True)[1]
@@ -71,17 +71,17 @@ def train_on_pool(choice_function, X, y,  pool_idcs, train_idcs, test_idcs, name
 		chosen_idx = choice_function(y_pool_p)
 
 		train_idcs.append( pool_idcs.pop(chosen_idx) )
-		
+
 	return n_points, accuracies, balances
 
-	
+
 def run_experiment():
 	np.random.seed(42)
 	classes = 3
 	X, y, pool_idcs, test_idcs, country_names, feature_names = dataprepare(bins=classes)
-	
+
 	print(f"pool size: {len(pool_idcs)}, test size: {len(test_idcs)}. Features: {feature_names}")
-	
+
 
 	methods = [random, least_conf, margin, entropy]
 	names = ["Random", "Least Confident", "Margin", "Entropy"]
@@ -89,8 +89,8 @@ def run_experiment():
 	#Start out with a country from each class
 	train_idcs = []
 	for c in range(classes):
-		for i, p in enumerate(pool_idcs): 
-			if y[p] == c: 
+		for i, p in enumerate(pool_idcs):
+			if y[p] == c:
 				print(f"Class {c} example: {country_names[p]} ")
 				train_idcs.append(pool_idcs.pop(i))
 				break
@@ -100,7 +100,7 @@ def run_experiment():
 		print(f"Running {names[i]}")
 		n_point, accuracy, balance = train_on_pool(choice_method, X, y, pool_idcs, train_idcs, test_idcs, names[i])
 		accuracies.append(accuracy); n_points.append(n_point); balances.append(balance)
-		
+
 
 	for i, accuracy in enumerate(accuracies):
 		plt.plot(n_points[i], accuracy)
@@ -120,7 +120,7 @@ def run_experiment():
 	plt.xlabel("Numer of Training Countries")
 	plt.ylabel(f"Training set balance: Largest proportion")
 	plt.show()
-	
+
 if __name__ == "__main__":
 	run_experiment()
 
