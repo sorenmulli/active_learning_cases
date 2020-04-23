0779eb6285bdc5ddace3177b06d51731ef9e0f84
sorenmulli <swholm@protonmail.com>
2020-04-23 15:34:48 +0200

    rename old + fix dataload

--- src/causality_reader.py
+++ src/causality_reader.py
@@ -13,22 +13,31 @@ from scipy.stats import ttest_ind, bartlett
 ####################
 
 def csv_read(path: str):
-	paths = glob(f'{path}/*.txt')
+	paths = glob(f'{path}/*.csv')
 	print(paths)
-	return [pd.read_csv(path, sep=" ") for path in paths]
+	dfs = list()
+	for path in paths:
+		df = pd.read_csv(path, header=0)
+		del df[df.columns[0]]
+		dfs.append(df)
+	return dfs
 
 def summary_analyze(data: pd.DataFrame, visual_pairs: list, condition: tuple):
 	if condition: data = data[data[condition[0]] == condition[1]]
-	print(data.shape)
+	print('shape', data.shape)
 	# Mean and std...
 	print(data.describe())
 
 	# Covariance matrix
-	plt.matshow(data.corr())
-	plt.show()
+	print(data.corr())
+	# plt.matshow(data.corr())
+	# plt.show()
 
 	# Joint and marginal
-	for pair in visual_pairs: sb.jointplot(*pair, data=data)
+	for pair in visual_pairs:
+		sb.jointplot(*pair, data=data)
+		plt.show()
+
 
 def compare(data_old: pd.DataFrame, data_new: pd.DataFrame, t_test_var: str):
 	# T test
@@ -44,7 +53,7 @@ if __name__ == "__main__":
 	datalist = csv_read('saved_data/causality')
 
 	condition = () # ex: ('X', 1)
-	data_check, visual_pairs = datalist[0], [('X','Y'), ('Z','F')]
+	data_check, visual_pairs = datalist[0], [('R','S'), ('S','T')]
 	summary_analyze(data_check, visual_pairs, condition)
 
 	# data_compares, test_var = (0, 1), 'X'
